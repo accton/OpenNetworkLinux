c6e8d2f3e53a844318a2360d314d00e93bc1e292
Eric Yang <eric_yang@accton.com>
2026-07-28 16:17:15 +0800

    [Accton] sfpi.c: unify restore-label syslog wording with as9726-32d
    Align the syslog message emitted after the restore: label in the
    QSFP-DD tx_disable control_set / control_get paths so it matches the
    as9726-32d convention:
    
        "Failed to restore Page Select to Admin Info on port(%d)!"
    
    Affected platforms:
    - as7926-40xfb
    - as9716_32d
    - as9817-64
    - as9817-64-nb
    - as9926-24d
    - as9926-24db
    
    Signed-off-by: Eric Yang <eric_yang@accton.com>

--- packages/platforms/accton/x86-64/as7926-40xfb/onlp/builds/x86_64_accton_as7926_40xfb/module/src/sfpi.c
+++ packages/platforms/accton/x86-64/as7926-40xfb/onlp/builds/x86_64_accton_as7926_40xfb/module/src/sfpi.c
@@ -335,8 +335,7 @@ onlp_sfpi_control_set(int port, onlp_sfp_control_t control, int value)
 
                     restore:
                         if (onlp_sfpi_dev_writeb(port, PORT_EEPROM_DEVADDR, QSFP_DD_EEPROM_OFFSET_PAGE_SELECT, QSFP_DD_PAGE_ADMIN_INFO) < 0) {
-                            syslog(LOG_ERR, "Unable to write tx_disable status to port(%d): write page to eeprom fail",
-                                    port);
+                            syslog(LOG_ERR, "Failed to restore Page Select to Admin Info on port(%d)!", port);
                         }
 
                         if (rv < 0) {
@@ -563,8 +562,7 @@ onlp_sfpi_control_get(int port, onlp_sfp_control_t control, int* value)
 
                 restore:
                     if (onlp_sfpi_dev_writeb(port, PORT_EEPROM_DEVADDR, QSFP_DD_EEPROM_OFFSET_PAGE_SELECT, QSFP_DD_PAGE_ADMIN_INFO) < 0) {
-                        syslog(LOG_ERR, "Unable to read tx_disable status from port(%d): write page to eeprom fail",
-                                port);
+                        syslog(LOG_ERR, "Failed to restore Page Select to Admin Info on port(%d)!", port);
                     }
 
                     if (rv < 0) {
